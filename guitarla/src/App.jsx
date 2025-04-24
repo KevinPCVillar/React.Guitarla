f3b4559d40790c8fd070bddebd1a91f159233a07
Kevin Villar <kevin.villar@delpunto.com>
2025-04-23 19:23:04 -0600

    feat - add Guitar, Header components and db.js files

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Header from './components/Header' 
+import Guitar from './components/Guitar'
+import { db } from './data/db'
 
 function App() {
 
+  /*
+  //State
+  const [auth, setAuth] = useState(false)
+  const [total, setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+
+  //Effect
+  useEffect(() => { 
+    if(auth){
+      console.log("Usuario autenticado")
+    }
+  }, [auth])
+*/
+
+const [data, setData] = useState([db])
+console.log(db);
   return (
     <>
-      <h1>Guitar LA</h1>
+
+    <Header />
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar />
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
     </>
   )
 }
